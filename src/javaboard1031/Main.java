3ce3a4da8d8932e26ff9c928dd10a52aebf6ef9e
chacha86 <taejincha0619@gmail.com>
2020-11-07 18:08:47 +0900

    게시물 수정/삭제

--- src/javaboard1031/Main.java
+++ src/javaboard1031/Main.java
@@ -1,51 +1,100 @@
 package javaboard1031;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+	static ArrayList<Article> articles = new ArrayList<>();
+
+	public static int getArticleIndexById(int aid) {
+		ArrayList<Article> articles = new ArrayList<>();
+
+		int existFlag = 1; // 1 없다, 2 있다.
+		int index = -1; // -1 없다.
+
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+			if (aid == article.getId()) {
+				existFlag = 2;
+				index = i;
+			}
+		}
+
+		return index;
+
+	}
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		String[] titles = new String[10];
-		String[] bodies = new String[10];
-		int[] ids = new int[10];
-		
-		int index = 0;
+
 		int id = 1;
-		
-		while(true) {
+
+		while (true) {
 			System.out.print("명령어를 입력해주세요 : ");
 			String cmd = sc.nextLine();
-			if(cmd.equals("exit")) {
+			if (cmd.equals("exit")) {
 				System.out.println("종료");
 				break;
 			}
-			
-			if(cmd.equals("add")) {
-					
+
+			if (cmd.equals("add")) {
+
 				System.out.print("게시물 제목을 입력해주세요 : ");
-				titles[index] = sc.nextLine();
+				String title = sc.nextLine();
 				System.out.print("게시물 내용을 입력해주세요 : ");
-				bodies[index] = sc.nextLine();
+				String body = sc.nextLine();
 				System.out.println("게시물이 등록되었습니다.");
-				ids[index] = id;
-				
-				index++;
+
+				Article article1 = new Article(id, title, body);
+				articles.add(article1);
 				id++;
-				
-			} 
-			if(cmd.equals("list")) {
-			
-				for(int i = 0; i < index; i++) {
-					System.out.println("번호 : " + ids[i]);
-					System.out.println("제목 : " + titles[i]);
-					System.out.println("===================");					
+
+			}
+			if (cmd.equals("list")) {
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					System.out.println("번호 : " + article.getId());
+					System.out.println("제목 : " + article.getTitle());
+					System.out.println("===================");
 				}
-				
+			}
+
+			if (cmd.equals("update")) {
+				System.out.print("수정할 게시물 번호 : ");
+				String aid = sc.nextLine();
+				int targetId = Integer.parseInt(aid);
+				int index = getArticleIndexById(targetId);
+
+				if (index == -1) {
+					System.out.println("없는 게시물입니다.");
+				} else {
+					System.out.print("제목 : ");
+					String title = sc.nextLine();
+					System.out.print("내용 : ");
+					String body = sc.nextLine();
+
+					Article article2 = new Article(targetId, title, body);
+
+					articles.set(index, article2);
+				}
+
+			}
+
+			if (cmd.equals("delete")) {
+				System.out.print("삭제할 게시물 번호 : ");
+				String aid = sc.nextLine();
+				int targetId = Integer.parseInt(aid);
+				int index = getArticleIndexById(targetId);
+				if (index == -1) {
+					System.out.println("없는 게시물입니다.");
+				} else {
+					articles.remove(index);
+					System.out.println("삭제가 완료되었습니다.");
+				}
+
 			}
 		}
-	
 	}
-
 }
--- src/javaboard1031/Test.java
+++ src/javaboard1031/Test.java
@@ -6,66 +6,17 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		int id = 1;
+		String s = "11";
 		
-		ArrayList<Article> articleList = new ArrayList<>();	
+		System.out.println(s + 10);
 		
-		Article a1 = new Article(id, "제목1", "내용1");
-		articleList.add(a1);
-		id++;
+		int a = Integer.parseInt(s);
 		
-		Article a2 = new Article(id, "제목2", "내용2");
-		articleList.add(a2);		
-		id++;
+		System.out.println(a);
+		System.out.println(a + 10);
 		
-		Article a3 = new Article(id, "제목3", "내용3");
-		articleList.add(a3);
-		id++;
-
-		articleList.remove(1);
 		
-		for(int i = 0; i < articleList.size(); i++) {
-			Article a = articleList.get(i);
-			System.out.println(a.getId());
-			System.out.println(a.getTitle());
-			System.out.println(a.getBody());
-			System.out.println("==========");
-		}
-		
+			
 	}
 
-}
-
-class Article {
-	private int id;
-	private String title;
-	private String body;
-	
-	public Article(int id, String title, String body) {
-		this.id = id;
-		this.title = title;
-		this.body = body;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getBody() {
-		return body;
-	}
-	public void setBody(String body) {
-		this.body = body;
-	}
-
-	
-	
 }
