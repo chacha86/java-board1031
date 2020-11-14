2c894c52cd5cd131867fb1acabd16fc3320ea779
jhs512 <jangka512@gmail.com>
2020-11-14 17:25:53 +0900

    ss

--- src/javaboard1031/Main.java
+++ src/javaboard1031/Main.java
@@ -135,40 +135,7 @@ public class Main {
 					System.out.println("작성자 : " + article.getWriter());
 					System.out.println("======================");
 				}
-			}
-			
-			if(cmd.equals("search")) {
-				
-				System.out.println("검색 항목을 선택해주세요 (1. 제목, 2. 내용, 3. 제목 + 내용, 4. 작성자) : ");
-				int searchTarget = Integer.parseInt(sc.nextLine());
-				
-				System.out.print("검색 키워드를 입력해주세요 : ");
-				String keyword = sc.nextLine();
-				
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					String targetStr = "";
-					if(searchTarget == 1) {
-						targetStr = article.getTitle();
-					} else if(searchTarget == 2) {
-						targetStr = article.getBody();
-					} else if(searchTarget == 3) {
-						targetStr = article.getTitle() + article.getBody();
-					} else {
-						targetStr = article.getWriter();
-					}
-					
-					if(targetStr.contains(keyword)) {
-						System.out.println("번호 : " + article.getId());
-						System.out.println("제목 : " + article.getTitle());
-						System.out.println("등록날짜 : " + article.getRegDate());
-						System.out.println("조회수 : " + article.getHit());
-						System.out.println("작성자 : " + article.getWriter());
-						System.out.println("===================");
-					}
-				}
-			}
-			
+			}			
 		}
 	}
 }
